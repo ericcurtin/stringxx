e67f99b57718b03fd646e5cd153a8b16a5e9f66d
Eric Curtin <eric.curtin@hpe.com>
2017-12-21 15:00:46 +0000

    lower and upper

--- string.cpp
+++ string.cpp
@@ -29,14 +29,14 @@ void cleanse(string& str) {
   str.clear();
 }
 
-void ltrim(string &str) {
+void ltrim(string& str) {
   str.erase(str.begin(),
             find_if(str.begin(),
                     str.end(),
                     not1(ptr_fun<int, int>(isspace))));
 }
 
-void rtrim(string &str) {
+void rtrim(string& str) {
   str.erase(find_if(str.rbegin(),
                     str.rend(),
                     not1(ptr_fun<int, int>(isspace))).base(),
@@ -46,5 +46,13 @@ void rtrim(string &str) {
 void trim(string &str) {
   ltrim(str);
   rtrim(str);
+}
+
+void lower(string& str) {
+  transform(str.begin(), str.end(), str.begin(), tolower);
+}
+
+void upper(string& str) {
+  transform(str.begin(), str.end(), str.begin(), toupper);
 }
 
